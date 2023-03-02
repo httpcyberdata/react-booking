88c6174be6602409502e935f46f5e7d251b7c87f
httpcyberdata <shieldedsafety@gmail.com>
2023-03-02 17:56:39 -0500

    chore: spaced and made gaps between items in the navigation bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,32 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBed } from '@fortawesome/free-solid-svg-icons';
+import { faBed, faPlane, faCar, faTaxi } from '@fortawesome/free-solid-svg-icons';
 import './header.css';
 
 const Header = () => {
 	return (
 		<div className="header">
-			<div className="headerList">
-				<div className="headerListItem">
-				Header
-					<FontAwesomeIcon icon={faBed} />
+			<div className="headerContainer">
+				<div className="headerList">
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faBed} />
+						<span>Stays</span>
+					</div>		
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faPlane} />
+						<span>Flights</span>
+					</div>		
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faCar} />
+						<span>Car rentals</span>
+					</div>		
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faBed} />
+						<span>Attractions</span>
+					</div>		
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faTaxi} />
+						<span>Airport taxis</span>
+					</div>
 				</div>
 			</div>
 		</div>
