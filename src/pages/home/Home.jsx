c2b336a1d4f283672671d713ec044ff39c0c3294
httpcyberdata <shieldedsafety@gmail.com>
2023-03-05 12:37:43 -0500

    chore: added propertyList component & styled w/ flexbox

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import './home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/PropertyList';
 
 const Home = () => {
 	return (
@@ -11,7 +12,10 @@ const Home = () => {
 			<Header />
 			<div className="homeContainer">
 				<Featured />
-				<Featured />
+				<h1 className="homeTitle">
+					Browse by property type
+				</h1>
+				<PropertyList />
 			</div>
 		</div>
 	)
