0532cf15f9bcc11d5b72f7390e9bb26bc4f141ad
httpcyberdata <shieldedsafety@gmail.com>
2023-03-07 22:09:41 -0500

    chore: created the search list item to show the calendary and exact dates

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -3,11 +3,18 @@ import './list.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import {format} from "date-fns";
+import { DateRange } from 'react-date-range';
+
+
 
 const List = () => {
 
 	const location = useLocation();
-	console.log(location)
+	const [destination, setDestination] = useState(location.state.destination);
+	const [date, setDate] = useState(location.state.date);
+	const [options, setOptions] = useState(location.state.options);
 
 	return (
 		<div>
@@ -21,15 +28,21 @@ const List = () => {
 						</h1>
 						<div className="lsItem">
 							<label>Destination</label>
-							<input type="text" />
+							<input placeholder={destination} type="text" />
 						</div>				
 						<div className="lsItem">
 							<label>Check-in Date</label>
-							<input type="text" />
+							<span>
+							{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}
+							</span>
+							<DateRange 
+								onChange={item => setDate([item.selection])} 
+								minDate={new Date()}
+								ranges={date}
+							/>
 						</div>
 					</div>
 					<div className="listResult">
-
 					</div>
 				</div>
 			</div>
