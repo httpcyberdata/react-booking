3e6fd7e930ccfb77d8fbb8a1dabf871fad86ed4c
httpcyberdata <shieldedsafety@gmail.com>
2023-03-09 11:19:56 -0500

    chore: created & styled SearchItem component

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -6,7 +6,7 @@ import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import {format} from "date-fns";
 import { DateRange } from 'react-date-range';
-
+import SearchItem from '../../components/searchItem/SearchItem';
 
 
 const List = () => {
@@ -105,6 +105,15 @@ const List = () => {
 						</button>
 					</div>
 					<div className="listResult">
+						<SearchItem />
+						<SearchItem />
+						<SearchItem />
+						<SearchItem />
+						<SearchItem />
+						<SearchItem />
+						<SearchItem />
+						<SearchItem />
+						<SearchItem />
 					</div>
 				</div>
 			</div>
