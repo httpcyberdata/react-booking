785ea51fd38f1a391b7b456e9a2579795633949a
httpcyberdata <shieldedsafety@gmail.com>
2023-03-11 10:17:02 -0500

    chore: created photo slide functionality via handleMove() onClick event & styled arrows in CSS

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -37,15 +37,32 @@ const Hotel = () => {
   	setOpen(true);
   }
 
+  const handleMove = (direction) => {
+  	let newSlideNumber;
+  	if(direction === "l") {
+  		newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+  	} else {
+  		newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+  	}
+
+  	setSlideNumber(newSlideNumber);
+  }
+
 	return (
 		<div>
 			<Navbar/>
 			<Header type="list" />
 			<div className="hotelContainer">
 				{open && <div className="slider">
-							<FontAwesomeIcon icon={faCircleXmark} />
-							<FontAwesomeIcon icon={faCircleArrowLeft} />
-							<FontAwesomeIcon icon={faCircleArrowRight} />
+							<FontAwesomeIcon icon={faCircleXmark} onClick={() => setOpen(false)}/>
+							<FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={() => handleMove("l")}/>
+							<div className="sliderWrapper">
+								<img 
+										src={photos[slideNumber].src} 
+										alt="" 
+										className="sliderImg" />
+							</div>
+							<FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={() => handleMove("r")}/>
 				</div>}
 				<div className="hotelWrapper">
 					<button className="bookNow">
@@ -69,7 +86,7 @@ const Hotel = () => {
 						<div className="hotelImages">
 							{photos.map((photo, i) => (
 								<div className="hotelImgWrapper">
-									<img onClick={handleOpen} src={photo.src} className="hotelImg" />
+									<img onClick={() => handleOpen(i)} src={photo.src} className="hotelImg" />
 								</div>
 							))}
 						</div>
